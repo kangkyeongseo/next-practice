dccdd3356ef3e5f27f61f3ad535be9b2877e48d9
kangkyeongseo <kks_big@naver.com>
2023-11-27 16:45:56 +0900

    feat: Create Data

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>create</>;
+  const router = useRouter();
+  const onSumbit = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    fetch(`http://localhost:9999/topics`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, body }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        const lastId = json.id;
+        router.push(`/read/${lastId}`);
+      });
+  };
+  return (
+    <form onSubmit={onSumbit}>
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
